5c588a9e230df5ac23ee677d7ca51e76d7c3a7a4
Lukas Bombach <lukas.bombach@googlemail.com>
2019-04-08 18:05:59 +0200

    runtime js wip

--- packages/super-intendent/runtime.js
+++ packages/super-intendent/runtime.js
@@ -1,3 +1,11 @@
-(() => {
-  console.log("runtime running");
-})();
+import ReactDOM from "react-dom";
+
+console.log("runtime running");
+
+const dataStr = document.querySelector(
+  'script[type="application/hydration-data"]'
+).innerHTML;
+
+const data = JSON.parse(dataStr);
+
+console.log(data);
