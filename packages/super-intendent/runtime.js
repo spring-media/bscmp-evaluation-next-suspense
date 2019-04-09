a86ec14c3167bd149844f9416f0c7af93863ba9a
Lukas Bombach <lukas.bombach@googlemail.com>
2019-04-09 16:56:20 +0200

    first working hydration

--- packages/super-intendent/runtime.js
+++ packages/super-intendent/runtime.js
@@ -1,15 +1,34 @@
+import React from "react";
 import ReactDOM from "react-dom";
+import Article from "../../components/article";
 
 if (typeof document !== undefined) {
   console.log("runtime running");
 
-  const dataStr = document.querySelector(
+  const hydrateComponents = [Article];
+
+  const componentMap = hydrateComponents.reduce((map, comp) => {
+    const compName = comp.displayName || comp.name;
+    return Object.assign(map, { [compName]: comp });
+  }, {});
+
+  const serializedHydrationData = document.querySelector(
     'script[type="application/hydration-data"]'
   ).innerHTML;
 
-  const data = JSON.parse(dataStr);
+  const hydrationData = JSON.parse(serializedHydrationData);
 
-  console.log(data);
+  Object.entries(hydrationData).forEach(([id, data]) => {
+    const marker = document.querySelector(
+      `script[type="application/hydration-marker"][id="${id}"]`
+    );
+    let container = marker;
+    data.reverse().forEach(({ name, props }) => {
+      const Element = componentMap[name];
+      container = container.previousElementSibling;
+      ReactDOM.hydrate(<Element {...props} />, container);
+    });
+  });
 } else {
   console.log("omitting runtime, running on the server");
 }
